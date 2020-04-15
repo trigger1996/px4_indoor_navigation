534025775bc89efffc53a31da53c9ab34d69c1f7
trigger1996 <529388771@qq.com>
2020-04-16 02:23:21 +0800

    fixed bug of unable to takeoff

--- src/topic_relay/topic_relay_main.cpp
+++ src/topic_relay/topic_relay_main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
-#include <mavros_msgs/Altitude.h>
+#include <sensor_msgs/Range.h>
 #include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
 
 using namespace std;
 
@@ -13,11 +14,11 @@ void laser_raw_cb(const sensor_msgs::LaserScan::ConstPtr& msg){
     is_laser_updated = true;
 }
 
-mavros_msgs::Altitude fcu_alt;
-bool is_fcu_alt_updated = false;
-void fcu_alt_cb(const mavros_msgs::Altitude::ConstPtr& msg){
-    fcu_alt = *msg;
-    is_fcu_alt_updated = true;
+sensor_msgs::Range external_alt;
+bool is_external_alt_updated = false;
+void external_alt_cb(const sensor_msgs::Range::ConstPtr& msg){
+    external_alt = *msg;
+    is_external_alt_updated = true;
 }
 
 geometry_msgs::Pose laser_slam_pose;
@@ -38,11 +39,11 @@ int main(int argc, char *argv[]) {
             ("/scan", 10);
 
     // robot pose integration
-    ros::Subscriber fcu_alt_sub = nh.subscribe<mavros_msgs::Altitude>
-            ("/mavros/altitude", 10, fcu_alt_cb);
+    ros::Subscriber fcu_alt_sub = nh.subscribe<sensor_msgs::Range>
+            ("/range", 10, external_alt_cb);
     ros::Subscriber laser_slam_pos_sub = nh.subscribe<geometry_msgs::Pose>
             ("/robot_pose", 10, laser_slam_pose_cb);
-    ros::Publisher slam_pose_3d_pub = nh.advertise<geometry_msgs::Pose>
+    ros::Publisher slam_pose_3d_pub = nh.advertise<geometry_msgs::PoseStamped>
             ("/mavros/vision_pose/pose", 10);
 
     ros::Rate rate(20.0);
@@ -62,12 +63,22 @@ int main(int argc, char *argv[]) {
             is_laser_updated = false;
         }
 
-        if (is_fcu_alt_updated && is_laser_slam_pose_updated) {
+        if (is_external_alt_updated && is_laser_slam_pose_updated) {
             // integrate 2D pose and alt to 3D
-            laser_slam_pose.position.z = fcu_alt.relative;
-            slam_pose_3d_pub.publish(laser_slam_pose);
+            geometry_msgs::PoseStamped pos_3d_to_pub;
+            pos_3d_to_pub.header.stamp = ros::Time::now();
+            pos_3d_to_pub.header.frame_id = "map";
 
-            is_fcu_alt_updated = false;
+            pos_3d_to_pub.pose.position.x = laser_slam_pose.position.x;
+            pos_3d_to_pub.pose.position.y = laser_slam_pose.position.y;
+            pos_3d_to_pub.pose.position.z = external_alt.range;
+
+            pos_3d_to_pub.pose.orientation.x = pos_3d_to_pub.pose.orientation.y =
+            pos_3d_to_pub.pose.orientation.z = pos_3d_to_pub.pose.orientation.w = 0.;
+
+            slam_pose_3d_pub.publish(pos_3d_to_pub);
+
+            is_external_alt_updated = false;
             is_laser_slam_pose_updated = false;
         }
 
